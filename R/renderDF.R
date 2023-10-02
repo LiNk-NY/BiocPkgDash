7d0c0ea86fd0059249dd089a5a69c6a6bfd862f5
LiNk-NY <marcel.ramos@roswellpark.org>
2023-10-02 18:08:15 -0400

    wip: build badge URLs

--- R/renderDF.R
+++ R/renderDF.R
@@ -15,6 +15,40 @@ renderDF <- function(email) {
     )
 }
 
+.SHIELDS_URL <- "http://bioconductor.org/shields/build/"
+
+renderDoc <- function(email) {
+    maindf <- biocMaintained(main = email)
+    sourceType <- vapply(maindf[["biocViews"]], `[[`, character(1L), 1L)
+    rsn <- BiocPkgTools:::repo_short_names
+    badgeKey <- rsn[rsn[["repository"]] == tolower(sourceType), "stat.url"]
+    rshields <- paste0(
+        .SHIELDS_URL, "release/", badgeKey, maindf[["Package"]], ".svg"
+    )
+    dshields <- paste0(
+        .SHIELDS_URL, "devel/", badgeKey, maindf[["Package"]], ".svg"
+    )
+    mdf <- cbind.data.frame(
+        maindf,
+        pkgurl = paste0("https://bioconductor.org/packages/", mdf[["Package"]])
+    )
+    datalist <- unname(split(adf, adf[["package"]]))
+    tableTemplate <- c(
+        "| Name | Bioc-release | Bioc-devel |",
+        "|:-----:|:-----:|:-----:|",
+        "{{#packages}}",
+        paste0("| [{{{package}}}]({{{pkgurl}}}) |",
+            " [![Bioconductor-release Build Status]({{{rshield}}})] |",
+            " [![Bioconductor-devel Build Status]({{{dshield}}})]"
+        ),
+        "{{/packages}}"
+    )
+    whisker.render(
+        template = tableTemplate,
+        data = list(packages = datalist)
+    )
+}
+
 .buildHTMLBadge <- function(package, version) {
     datdf <- data.frame(package = package, version = version)
     shieldtemp <- paste0(
@@ -33,7 +67,7 @@ renderDF <- function(email) {
     )
     landing <- whisker::whisker.render(landtemp, data = datdf)
     paste0(
-        '<a href=', dQuote(landing), ' target="_blank" rel = "noopener">',
+        '<a href=', dQuote(landing), ' target="_blank">',
         '<img src=', dQuote(shieldurl), alttxt
     )
 }
