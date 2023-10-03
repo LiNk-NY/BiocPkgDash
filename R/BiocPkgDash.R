1d184f86842f7961d90b29a761e06d88e929bd54
LiNk-NY <marcel.ramos@roswellpark.org>
2023-10-03 16:51:14 -0400

    add Bioconductor branding

--- R/BiocPkgDash.R
+++ R/BiocPkgDash.R
@@ -44,6 +44,17 @@ BiocPkgDash <- function(...) {
     }
 
     ui <- fluidPage(
+        titlePanel(
+            windowTitle = "BiocPkgDash",
+            title = div(
+                img(
+                    src = "images/bioconductor_logo_rgb_small.png",
+                    align = "right",
+                    style = "margin-right:10px"
+                ),
+                h1(id = "big-heading", "Bioconductor Package Dashboard")
+            )
+        ),
         emailField("email1")
     )
 
